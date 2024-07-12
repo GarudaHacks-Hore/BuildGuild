e14d50ce4c7b6a07fab56824aa5cee547034fbf7
safiqq <szarifin20041@gmail.com>
2024-07-13 04:12:01 +0700

    feat: add loading for prompts

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,25 +1,26 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { supabase } from "@/lib/supabase";
-import dayjs from "dayjs";
-import PromptRoom from "@/components/PromptRoom";
-import { PromptHistory } from "@/types/PromptHistory";
-import { Message } from "@/types/Message";
-import axios from "axios";
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { supabase } from '@/lib/supabase';
+import dayjs from 'dayjs';
+import PromptRoom from '@/components/PromptRoom';
+import { PromptHistory } from '@/types/PromptHistory';
+import { Message } from '@/types/Message';
+import axios from 'axios';
 
-import { User } from "@supabase/supabase-js";
-import PromptList from "@/components/PromptList";
+import { User } from '@supabase/supabase-js';
+import PromptList from '@/components/PromptList';
 
 export default function Discover() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [todayPrompts, setTodayPrompts] = useState<PromptHistory[]>([]);
   const [yesterdayPrompts, setYesterdayPrompts] = useState<PromptHistory[]>([]);
   const [previousPrompts, setPreviousPrompts] = useState<PromptHistory[]>([]);
   const [messages, setMessages] = useState<Message[]>([]);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
   const [selectedItemId, setSelectedItemId] = useState<string | null>(null);
   const [user, setUser] = useState<User>();
   const [userId, setUserId] = useState<number>();
@@ -41,18 +42,18 @@ export default function Discover() {
       const { data, error } = await supabase.auth.getUser();
       if (!error) {
         const { data: profile, error: profileError } = await supabase
-          .from("profiles")
-          .select("bio")
-          .eq("email", data.user?.email)
+          .from('profiles')
+          .select('bio')
+          .eq('email', data.user?.email)
           .single();
 
         if (profileError || !profile) {
-          router.push("/sign-in");
+          router.push('/sign-in');
         } else if (profile.bio == null) {
-          router.push("/survey");
+          router.push('/survey');
         }
       } else {
-        router.push("/sign-in");
+        router.push('/sign-in');
       }
     };
     fetchUser();
@@ -61,10 +62,7 @@ export default function Discover() {
   useEffect(() => {
     const fetchPromptHistories = async () => {
       if (user) {
-        let result_profile = await supabase
-          .from("profiles")
-          .select("*")
-          .eq("email", user.email);
+        let result_profile = await supabase.from('profiles').select('*').eq('email', user.email);
 
         if (result_profile.error || result_profile.data.length == 0) {
           return;
@@ -73,9 +71,9 @@ export default function Discover() {
         setUserId(result_profile.data[0].id);
 
         const result_prompt = await supabase
-          .from("prompt_histories")
-          .select("*")
-          .eq("user", result_profile.data[0].id);
+          .from('prompt_histories')
+          .select('*')
+          .eq('user', result_profile.data[0].id);
 
         if (result_prompt.error) {
           return;
@@ -86,17 +84,17 @@ export default function Discover() {
         const previous: PromptHistory[] = [];
 
         const now = dayjs();
-        const startOfToday = now.startOf("day");
-        const startOfYesterday = startOfToday.subtract(1, "day");
+        const startOfToday = now.startOf('day');
+        const startOfYesterday = startOfToday.subtract(1, 'day');
 
         result_prompt.data.forEach((prompt: PromptHistory) => {
           const createdAt = dayjs(prompt.created_at);
 
-          if (createdAt.isSame(startOfToday, "day")) {
+          if (createdAt.isSame(startOfToday, 'day')) {
             today.push(prompt);
-          } else if (createdAt.isSame(startOfYesterday, "day")) {
+          } else if (createdAt.isSame(startOfYesterday, 'day')) {
             yesterday.push(prompt);
-          } else if (createdAt.isAfter(startOfToday.subtract(30, "day"))) {
+          } else if (createdAt.isAfter(startOfToday.subtract(30, 'day'))) {
             previous.push(prompt);
           }
         });
@@ -118,13 +116,10 @@ export default function Discover() {
   const handleClick = async (id: string) => {
     setSelectedItemId(id);
 
-    const { data, error } = await supabase
-      .from("prompts")
-      .select("*")
-      .eq("roomId", id);
+    const { data, error } = await supabase.from('prompts').select('*').eq('roomId', id);
 
     if (error) {
-      console.error("Error fetching prompt histories: ", error);
+      console.error('Error fetching prompt histories: ', error);
       return;
     } else {
       setMessages(data);
@@ -132,29 +127,30 @@ export default function Discover() {
   };
 
   const handleSendMessage = async (message: string) => {
-    setInputValue("");
+    setInputValue('');
     if (!selectedItemId) {
       const { data, error } = await supabase
-        .from("prompt_histories")
+        .from('prompt_histories')
         .insert({ user: userId, message })
-        .select("*");
+        .select('*');
 
       if (error) {
-        console.error("Error insert prompt histories: ", error);
+        console.error('Error insert prompt histories: ', error);
         return;
       } else {
         const { data: newMsgData, error: newMsgError } = await supabase
-          .from("prompts")
-          .insert({ chat: message, roomId: data[0].id, role: "user" })
-          .select("*");
+          .from('prompts')
+          .insert({ chat: message, roomId: data[0].id, role: 'user' })
+          .select('*');
 
         if (newMsgError) {
-          console.error("Error insert prompt histories: ", newMsgError);
+          console.error('Error insert prompt histories: ', newMsgError);
           return;
         } else {
           setSelectedItemId(data[0].id);
           setMessages(newMsgData ? newMsgData : []);
 
+          setIsLoading(true);
           // LLM
           const response = await axios.post(
             "http://192.168.177.120:8000/find",
@@ -173,22 +169,23 @@ export default function Discover() {
           } catch (error) {
             console.error(error);
           }
+          setIsLoading(false);
         }
       }
     } else {
       const { data, error } = await supabase
-        .from("prompts")
-        .insert({ chat: message, roomId: messages[0].roomId, role: "user" })
-        .select("*");
+        .from('prompts')
+        .insert({ chat: message, roomId: messages[0].roomId, role: 'user' })
+        .select('*');
 
       if (error) {
-        console.error("Error insert prompt histories: ", error);
+        console.error('Error insert prompt histories: ', error);
         return;
       } else {
         setMessages([...messages, data[0]]);
 
         // LLM
-        const response = await axios.post("http://192.168.177.120:8000/find", {
+        const response = await axios.post('http://192.168.177.120:8000/find', {
           prompt: message,
           identifier: data[0].roomId,
         });
@@ -204,12 +201,12 @@ export default function Discover() {
 
   return (
     <main
-      style={{ height: "calc(100vh - 80px)" }}
-      className="flex flex-col items-center justify-center gap-3"
+      style={{ height: 'calc(100vh - 80px)' }}
+      className='flex flex-col items-center justify-center gap-3'
     >
-      <div className="flex items-center w-full h-full">
-        <div className="w-1/5 px-10 py-4 flex flex-col justify-start h-full gap-6">
-          <h1 className="text-2xl font-bold mb-2">Discover People</h1>
+      <div className='flex items-center w-full h-full'>
+        <div className='w-1/5 px-10 py-4 flex flex-col justify-start h-full gap-6'>
+          <h1 className='text-2xl font-bold mb-2'>Discover People</h1>
           <Button
             onClick={() => {
               setSelectedItemId(null);
@@ -219,9 +216,9 @@ export default function Discover() {
             New chat
           </Button>
           {todayPrompts.length > 0 && (
-            <div className="flex flex-col gap-2">
-              <h2 className="text-xs font-light">Today</h2>
-              <div className="flex flex-col gap-1">
+            <div className='flex flex-col gap-2'>
+              <h2 className='text-xs font-light'>Today</h2>
+              <div className='flex flex-col gap-1'>
                 {todayPrompts.map((prompt) => (
                   <PromptList
                     key={prompt.id}
@@ -236,8 +233,8 @@ export default function Discover() {
             </div>
           )}
           {yesterdayPrompts.length > 0 && (
-            <div className="flex flex-col gap-2">
-              <h2 className="text-xs font-light">Yesterday</h2>
+            <div className='flex flex-col gap-2'>
+              <h2 className='text-xs font-light'>Yesterday</h2>
               <div>
                 {yesterdayPrompts.map((prompt) => (
                   <PromptList
@@ -253,8 +250,8 @@ export default function Discover() {
             </div>
           )}
           {previousPrompts.length > 0 && (
-            <div className="flex flex-col gap-2">
-              <h2 className="text-xs font-light">Previous 30 days</h2>
+            <div className='flex flex-col gap-2'>
+              <h2 className='text-xs font-light'>Previous 30 days</h2>
               <div>
                 {previousPrompts.map((prompt) => (
                   <PromptList
@@ -270,32 +267,37 @@ export default function Discover() {
             </div>
           )}
         </div>
-        <div className="bg-white flex flex-col shadow-lg gap-2 rounded-l-3xl p-6 w-4/5 h-full">
-          <div className="border rounded-lg p-4 flex-grow overflow-y-auto bg-gray-50">
+        <div className='bg-white flex flex-col shadow-lg gap-2 rounded-l-3xl p-6 w-4/5 h-full'>
+          <div className='border rounded-lg p-4 flex-grow overflow-y-auto bg-gray-50'>
             <PromptRoom
               setSelectedItemId={setSelectedItemId}
               setMessages={setMessages}
               selectedTab={selectedItemId}
               messages={messages}
             />
+            {isLoading && (
+              <div className='flex justify-start ml-4'>
+                <span className='animate-pulse inline-flex w-6 h-6 rounded-full bg-black opacity-75' />
+              </div>
+            )}
           </div>
           <form
             onSubmit={(e) => {
               e.preventDefault();
               handleSendMessage(inputValue);
             }}
-            className="flex items-center"
+            className='flex items-center'
           >
             <Input
-              type="text"
-              placeholder="Type your message..."
+              type='text'
+              placeholder='Type your message...'
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
               disabled={user === null}
             />
             <Button
-              type="submit"
-              className="ml-2"
+              type='submit'
+              className='ml-2'
               disabled={user === null || inputValue.length === 0}
             >
               Send
--- components/SurveyRoom.tsx
+++ components/SurveyRoom.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import SurveyChat from './SurveyChat';
 import axios from 'axios';
-import { supabase } from "@/lib/supabase";
+import { supabase } from '@/lib/supabase';
 import { User } from '@supabase/auth-js';
 
 interface Message {
@@ -20,10 +20,12 @@ interface SurveyRoomProps {
 }
 
 const SurveyRoom: React.FC<SurveyRoomProps> = ({ setPhase }) => {
-  const [messages, setMessages] = useState<Message[]>([{
-    id: 1,
-    role: 'assistant',
-    content: `
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      id: 1,
+      role: 'assistant',
+      content: `
 
     Hello friend! I'm excited to help you connect with people working on fascinating projects.
     Let's kick off your journey right here. Creating your profile will only take a minute, I promise.
@@ -32,16 +34,17 @@ const SurveyRoom: React.FC<SurveyRoomProps> = ({ setPhase }) => {
     
     What's your name and where are you based? Please include your city and country.
     
-    (For example, "Hi, I'm Farza from San Francisco, USA.")
+    (For example, "Hi, I'm Gerald from South Tangerang, Indonesia.")
     
     `,
-    created_at: new Date(),
-  }]);
+      created_at: new Date(),
+    },
+  ]);
   const [newMessage, setNewMessage] = useState('');
   const [phase, updatePhase] = useState<number>(1);
-  const [name, setName] = useState<string>("");
-  const [project, setProject] = useState<string>("");
-  const [milestone, setMilestone] = useState<string>("");
+  const [name, setName] = useState<string>('');
+  const [project, setProject] = useState<string>('');
+  const [milestone, setMilestone] = useState<string>('');
   const [user, setUser] = useState<User | null>(null);
 
   const handleSendMessage = async () => {
@@ -61,17 +64,15 @@ const SurveyRoom: React.FC<SurveyRoomProps> = ({ setPhase }) => {
         .map((message) => ({ role: message.role, content: message.content }))
     );
 
-    const response = await axios.post(
-      "http://192.168.177.120:8000/registration",
-      {
-        prompt: newMessage,
-        phase: phase,
-        conversation_history: [
-          ...messages,
-          userMessage,
-        ].filter((msg) => msg.id != 1).map((message) => ({ role: message.role, content: message.content })),
-      }
-    );
+    setIsLoading(true);
+    const response = await axios.post('http://172.25.118.194:8000/registration', {
+      prompt: newMessage,
+      phase: phase,
+      conversation_history: [...messages, userMessage]
+        .filter((msg) => msg.id != 1)
+        .map((message) => ({ role: message.role, content: message.content })),
+    });
+    setIsLoading(false);
 
     if (phase === 1) {
       setName(response.data.summary);
@@ -106,11 +107,11 @@ const SurveyRoom: React.FC<SurveyRoomProps> = ({ setPhase }) => {
     const submitProfile = async () => {
       if (phase === 4 && user) {
         const { error } = await supabase
-          .from("profiles")
+          .from('profiles')
           .update({ name: name, current_project: project, current_project_milestone: milestone })
-          .eq("email", user?.email);
+          .eq('email', user?.email);
         if (error) {
-          console.error("Error inserting profile: ", error);
+          console.error('Error inserting profile: ', error);
         }
       }
     };
@@ -127,6 +128,11 @@ const SurveyRoom: React.FC<SurveyRoomProps> = ({ setPhase }) => {
             message={message}
           />
         ))}
+        {isLoading && (
+          <div className='flex justify-start ml-4'>
+            <span className='animate-pulse inline-flex w-6 h-6 rounded-full bg-black opacity-75' />
+          </div>
+        )}
       </div>
       <form
         onSubmit={(e) => {
