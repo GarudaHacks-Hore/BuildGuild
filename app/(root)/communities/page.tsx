a1d06d1cce3fc368d725b135c12fb71cb063ebe3
safiqq <szarifin20041@gmail.com>
2024-07-12 15:03:09 +0700

    feat: create realtime chat

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import ChatRoom from "@/components/ChatRoom";
-import TopicFilter from "@/components/TopicFilter";
-import { GroupList } from "@/components/GroupList";
-import React, { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
-import { Group } from "@/types/Group";
+import ChatRoom from '@/components/ChatRoom';
+import TopicFilter from '@/components/TopicFilter';
+import { GroupList } from '@/components/GroupList';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase';
+import { Group } from '@/types/Group';
 
 export default function Communities() {
   const [groups, setGroups] = useState<Group[]>([]);
@@ -14,9 +14,9 @@ export default function Communities() {
 
   useEffect(() => {
     async function fetchGroups() {
-      const { data, error } = await supabase.from("groups").select("*");
+      const { data, error } = await supabase.from('groups').select('*');
       if (error) {
-        console.error("Error fetching groups: ", error);
+        console.error('Error fetching groups: ', error);
       } else {
         setGroups(data);
         setSelectedGroup(data[0]);
@@ -28,20 +28,23 @@ export default function Communities() {
 
   return (
     <main
-      style={{ height: "calc(100vh - 80px)" }}
-      className="flex flex-col items-center justify-center gap-3"
+      style={{ height: 'calc(100vh - 80px)' }}
+      className='flex flex-col items-center justify-center gap-3'
     >
-      <div className="flex items-center w-full h-full">
-        <div className="w-1/5 px-10 py-4 flex flex-col justify-start h-full gap-6">
-          <h1 className="text-2xl font-bold mb-2">Communities</h1>
-          <TopicFilter setFilteredGroups={setFilteredGroups} groups={groups} />
+      <div className='flex items-center w-full h-full'>
+        <div className='w-1/5 px-10 py-4 flex flex-col justify-start h-full gap-6'>
+          <h1 className='text-2xl font-bold mb-2'>Communities</h1>
+          <TopicFilter
+            setFilteredGroups={setFilteredGroups}
+            groups={groups}
+          />
           <GroupList
             groups={filteredGroups}
             selectedGroup={selectedGroup}
             setSelectedGroup={setSelectedGroup}
           />
         </div>
-        <div className="bg-white flex flex-col shadow-lg gap-2 rounded-l-3xl p-6 w-4/5 h-full">
+        <div className='bg-white flex flex-col shadow-lg gap-2 rounded-l-3xl p-6 w-4/5 h-full'>
           <ChatRoom group={selectedGroup} />
         </div>
       </div>
